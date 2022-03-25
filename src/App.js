f12af3215b0dc01521cc83c2b3a7a95a77ef0fbe
Jericho-West <97463076+Jericho-West@users.noreply.github.com>
2022-03-25 17:20:55 -0400

    got the names

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Char from './components/Character';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +11,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  let [slice0, changeSlice0] = useState([]);
+
+
+  useEffect(() => {
+  axios.get("https://swapi.dev/api/people/")
+    .then(x => {
+      changeSlice0 (x.data)
+    })
+    .catch(err => {
+      console.log(err)
+      })
+  }, []); 
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+     
+      <Char slice0={slice0}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import App from "../App";
+
+function Char(slice0) {
+    let slice = slice0
+    console.log("A")
+    console.log(slice.slice0)
+    console.log("a")
+    let a = <div></div>;
+    let b = []
+    
+    slice.slice0.forEach(element => {
+        a = <div>{element.name}</div>
+        b.push(a)
+        });
+
+        return b
+}
+
+export default Char
